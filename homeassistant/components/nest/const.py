618d3a0ec37285d150b7ab1ce8ba0c24face0aa9
Nick Mayer <nick.lastacorn.com>
2023-07-20 10:43:02 -0600

    Change nest component to use FT

--- homeassistant/components/nest/const.py
+++ homeassistant/components/nest/const.py
@@ -20,11 +20,11 @@ SIGNAL_NEST_UPDATE = "nest_update"
 
 # For the Google Nest Device Access API
 OAUTH2_AUTHORIZE = (
-    "https://nestservices.google.com/partnerconnections/{project_id}/auth"
+    "https://sdmresourcepicker-preprod.sandbox.google.com/partnerconnections/{project_id}/auth"
 )
 OAUTH2_TOKEN = "https://www.googleapis.com/oauth2/v4/token"
 SDM_SCOPES = [
     "https://www.googleapis.com/auth/sdm.service",
     "https://www.googleapis.com/auth/pubsub",
 ]
-API_URL = "https://smartdevicemanagement.googleapis.com/v1"
+API_URL = "https://preprod-smartdevicemanagement.googleapis.com/v1"
